c19aea19a5768a3ec70fd28a843feeb073b5f56d
Anadi Pandey <anadikrishnapandey@gmail.com>
2020-08-17 16:17:21 +0530

    i just added comment

--- largest_number.py
+++ largest_number.py
@@ -8,10 +8,9 @@ def largest_number(a):
     for x in a:
         res += x
     return res
-
+    #adding a comment to check workflow
 if __name__ == '__main__':
     input = sys.stdin.read()
     data = input.split()
     a = data[1:]
     print(largest_number(a))
-    
